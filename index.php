1a720586691ac9b73aec2c10768efc4ced3621e7
Juan <villasmiljuandiego@gmail.com>
2023-03-09 00:06:58 -0400

    ejercicio3

--- index.php
+++ index.php
@@ -38,6 +38,7 @@ unset($_POST['folder']);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="shortcut icon" href="./assets/img/notas.png" />
 </head>
 
 <body>
